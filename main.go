7d5b55f1b0cdf5d5e7f5ae56c92d4ab72f492d54
Ismail <ismailsmakhus@gmail.com>
2020-04-13 13:15:11 +0700

    add echo server with some routes

--- main.go
+++ main.go
@@ -1,24 +1,42 @@
 package main
 
-import "log"
-import "os"
-import "github.com/gin-gonic/gin"
-import "github.com/joho/godotenv"
+import (
+	"govote/models"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	admincontroller "govote/controllers/admin"
+	votercontroller "govote/controllers/voter"
+)
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Println("Error loading .env file")
-    }
-    
-    username := os.Getenv("APP_USERNAME")
-    password := os.Getenv("APP_PASSWORD")
-    
-    log.Println(username + " " + password)
-    
-    app := gin.Default()
-    app.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{"status": "OK"})
-    })
-    app.Run()
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	models.Init()
+
+	app := echo.New()
+
+	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${time_rfc3339} ${method} ${uri} ${status} ${latency_human}\n",
+	}))
+	app.Use(middleware.Recover())
+
+	admin := app.Group("/api/admin")
+	admin.POST("/login", admincontroller.Login)
+	admin.GET("/votes", admincontroller.ListVotes)
+	admin.POST("/votes", admincontroller.CreateVotes)
+	admin.GET("/voters", admincontroller.ListVoters)
+	admin.POST("/voters", admincontroller.CreateVoters)
+
+	voter := app.Group("/api/voter")
+	voter.POST("/login", votercontroller.Login)
+	voter.POST("/vote", votercontroller.Vote)
+
+	app.Start(":8080")
 }
